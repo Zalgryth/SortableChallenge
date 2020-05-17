6392de7ae516ba946056b573e20559d0a64e65cc
Nicolas Lavallee <zalgryth@gmail.com>
2020-05-17 17:34:05 -0400

    Extract auction winning bid compution to own module.

--- auction/auction.py
+++ auction/auction.py
@@ -1,4 +1,7 @@
-from typing import List
+from typing import List, Dict
+import json
+
+from .config import Config
 
 
 class Bid(object):
@@ -17,3 +20,52 @@ class Auction(object):
         self.site = site
         self.units = units
         self.bids = bids
+
+
+class AuctionHelper(object):
+    """Contains helper methods for calculating winning bids for an auction."""
+
+    def __init__(self, config: Config):
+        self._config = config
+
+        # TODO: Find a better way to do a .ToDictionary() equivalent in python
+        self._bidder_adjustments: Dict[str, float] = {}
+        for bidder in config.bidders:
+            self._bidder_adjustments[bidder.name] = bidder.adjustment
+
+    def get_winning_bids(self, auction: Auction) -> List[Bid]:
+        """Get winning bids.
+
+        :param auction: The auction to perform winning big computation on.
+        :return: Returns a list of winnings bids (per unit) for the auction.
+        """
+        winning_bids: List[Bid] = []
+
+        print("doing auction for site " + auction.site)
+
+        # Get configuration for the current site based on the auction name.
+        site_config = next(
+            filter(lambda i: i.name == auction.site, self._config.sites))
+
+        for unit in auction.units:
+            unit_bids = list(filter(lambda i: i.unit == unit and
+                                    i.bidder in site_config.bidders,
+                                    auction.bids))
+
+            # Adjustments, though based on averages, are estimates. The floor
+            # should only be calculated based on the actual bid, not the payout.
+            unit_bids = list(filter(lambda i:
+                                    i.bid >= site_config.floor,
+                                    unit_bids))
+
+            # Order the bids by their adjusted value.
+            sortedBids = sorted(unit_bids, key=self.get_adjusted_value,
+                                reverse=True)
+
+            # TODO: What if the list is empty?
+            winning_bids.append(sortedBids[0])
+
+        return winning_bids
+
+    def get_adjusted_value(self, bid: Bid):
+        return bid.bid + (bid.bid * self._bidder_adjustments[bid.bidder])
--- auction/main.py
+++ auction/main.py
@@ -5,18 +5,20 @@ import pathlib
 from typing import List, Dict
 
 from . import json_encoder
-from .auction import Auction, Bid
+from .auction import AuctionHelper, Auction, Bid
 from .config import Config, Bidder, Site
 
 
 def main():
     config = get_config()
-    auctions = getAuctions()
+    auctions = get_auctions()
+
+    auction_helper = AuctionHelper(config)
 
     winning_bids: List[Bid] = []
 
     for auction in auctions:
-        winning_bids.append(get_winning_bids(auction, config))
+        winning_bids.append(auction_helper.get_winning_bids(auction))
 
     print_json(winning_bids)
 
@@ -31,56 +33,6 @@ def print_json(data):
     print(json.dumps(data, indent=4, cls=json_encoder.DefaultEncoder))
 
 
-def get_winning_bids(auction: Auction, config: Config) -> List[Bid]:
-    """
-
-    :param auction:
-    :param config:
-    :return:
-    """
-    # return an array of winning bids for the auction
-    winning_bids: List[Bid] = []
-
-    print("doing auction for site " + auction.site)
-
-    # Get configuration for the current site based on the auction name.
-    site_config = next(filter(lambda i: i.name == auction.site, config.sites))
-
-    # TODO: Find a better way to do a .ToDictionary() equivalent in python
-    bidder_adjustments = {}
-    for bidder in config.bidders:
-        bidder_adjustments[bidder.name] = bidder.adjustment
-
-    for unit in auction.units:
-        unit_bids = list(filter(lambda i: i.unit == unit and
-                                i.bidder in site_config.bidders,
-                                auction.bids))
-
-        # Adjustments, though based on averages, are estimates. The floor
-        # should only be calculated based on the actual bid, not the payout.
-        unit_bids = list(filter(lambda i:
-                                i.bid >= site_config.floor,
-                                unit_bids))
-
-        # Order the bids by their adjusted value.
-        sortedBids = sorted(unit_bids,
-                            key=lambda x:
-                            get_adjusted_value(x, bidder_adjustments),
-                            reverse=True)
-
-        print("Final sorted bids:")
-        print_json(sortedBids)
-
-        # TODO: What if the list is empty?
-        winning_bids.append(sortedBids[0])
-
-    return winning_bids
-
-
-def get_adjusted_value(bid: Bid, bidder_adjustments: Dict[str, float]):
-    return bid.bid + (bid.bid * bidder_adjustments[bid.bidder])
-
-
 def get_config() -> Config:
     with open(pathlib.Path(__file__).parent / './config.json') as config_file:
         config_json_str = config_file.read()
@@ -91,7 +43,7 @@ def get_config() -> Config:
                   [Bidder("AUCT", -0.0625), Bidder("BIDD", 0)])
 
 
-def getAuctions() -> Auction:
+def get_auctions() -> Auction:
     json_str = sys.stdin.read()
 
     # TODO: Deserialize JSON into objects
